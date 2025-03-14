9c6e397676d9aa98aa7192879321ba31f411a7cd
abobqa <simochkoya@mail.ru>
2025-03-14 17:44:05 +0300

    zadanie 4

--- client.py
+++ client.py
@@ -1,16 +1,28 @@
-import socket
-from time import sleep
+import socket  
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
+sock.setblocking(1)
+print("КЛИЕНТ: Попытка соединения с сервером на адресе localhost:9090...")
+sock.connect(('localhost', 9090))
+print("КЛИЕНТ: Соединение с сервером установлено.")
+
+
+msg = input("Введите строку для отправки серверу: ")
+
+print("КЛИЕНТ: Отправка данных серверу...")
+
 sock.send(msg.encode())
+print("КЛИЕНТ: Данные успешно отправлены серверу.")
+
+print("КЛИЕНТ: Ожидание ответа от сервера...")
 
 data = sock.recv(1024)
 
-sock.close()
+print("КЛИЕНТ: Данные получены от сервера.")
 
-print(data.decode())
+
+sock.close()
+print("КЛИЕНТ: Соединение с сервером закрыто.")
+
+print("Ответ от сервера:", data.decode())
